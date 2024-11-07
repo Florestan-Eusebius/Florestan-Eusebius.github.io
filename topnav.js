1d9ac07d2b58e630172198b0f9999f5c335410b4
Wang Yifei <wang_yff@outlook.com>
2024-11-07 20:49:59 +0800

    modified:   topnav.js

--- topnav.js
+++ topnav.js
@@ -1,38 +1,41 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-
-// window.onclick = function(event) {
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
+  
+// function handleEvent(event) {
 //   if (document.getElementsByClassName('icon')[0].contains(event.target)) {
 //     myFunction();
 //   } else {
 //     var x = document.getElementById("myTopnav");
 //     x.className = "topnav";
 //   }
-//   event.stopPropagation()
+//   event.stopPropagation();
 // }
 
-// window.addEventListener("click", function(event){
-//   if (document.getElementsByClassName('icon')[0].contains(event.target)) {
-//         myFunction();
-//       } else {
-//         var x = document.getElementById("myTopnav");
-//         x.className = "topnav";
-//       }
-//       event.stopPropagation()
-// });
+// window.addEventListener('click', handleEvent);
+// window.addEventListener('touchstart', handleEvent);
+  
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
+  }
+}
 
 function handleEvent(event) {
   if (document.getElementsByClassName('icon')[0].contains(event.target)) {
-    myFunction();
+      myFunction();
+      event.preventDefault(); // Prevent default behavior only for the icon
   } else {
-    var x = document.getElementById("myTopnav");
-    x.className = "topnav";
+      var x = document.getElementById("myTopnav");
+      x.className = "topnav";
   }
   event.stopPropagation();
 }
@@ -40,3 +43,24 @@ function handleEvent(event) {
 window.addEventListener('click', handleEvent);
 window.addEventListener('touchstart', handleEvent);
 
+
+
+  // window.onclick = function(event) {
+  //   if (document.getElementsByClassName('icon')[0].contains(event.target)) {
+  //     myFunction();
+  //   } else {
+  //     var x = document.getElementById("myTopnav");
+  //     x.className = "topnav";
+  //   }
+  //   event.stopPropagation()
+  // }
+  
+  // window.addEventListener("click", function(event){
+  //   if (document.getElementsByClassName('icon')[0].contains(event.target)) {
+  //         myFunction();
+  //       } else {
+  //         var x = document.getElementById("myTopnav");
+  //         x.className = "topnav";
+  //       }
+  //       event.stopPropagation()
+  // });
